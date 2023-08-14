786967fdb05fe78ccf03e3a765319503dc4afdbb
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-08-14 11:03:53 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { SITE_NAME } from '$lib/constants'
+
+    import '@fontsource-variable/playfair-display'
+    import '@fontsource-variable/rosario'
+    import '@skeletonlabs/skeleton/themes/theme-skeleton.css'
+    import '@skeletonlabs/skeleton/styles/skeleton.css'
+    import '../app.css'
+</script>
+
+<svelte:head>
+    <title>{SITE_NAME}</title>
+</svelte:head>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<div class="mx-auto max-w-screen-lg mt-8 px-4">
+    <h1 class="h1 font-bold mb-4">Reconnect.earth</h1>
+
+    <p class="text-lg">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+        quibusdam fugit et. Expedita veniam eveniet nam id similique earum
+        suscipit accusamus facere laborum? Libero rem id, eligendi fugiat cum
+        harum.
+    </p>
+</div>

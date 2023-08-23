f35f6f34f232ef1ffbf36a7b26c0454d7e7d29d6
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-08-23 20:37:13 +0200

    Add explore link

--- src/components/Resources.svelte
+++ src/components/Resources.svelte
@@ -23,6 +23,27 @@
                     <span class="text-sm">#{tag}</span>
                 {/each}
             </div>
+            <div class="flex justify-end mt-4">
+                <a
+                    class="btn variant-filled btn-sm rounded-sm gap-1"
+                    href={resource.link}
+                    target="_blank"
+                    ><span>Explore</span><svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
+                        />
+                    </svg></a
+                >
+            </div>
         </div>
     {/each}
 </div>

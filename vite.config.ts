ae59082a0e5a27d6162f4df3f55a2dffa198b8c5
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-10-03 22:47:52 +0200

    Add embedded lifewheel app

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,17 @@ import { defineConfig } from 'vite'
 import Icons from 'unplugin-icons/vite'
 import purgeCSS from 'vite-plugin-tailwind-purgecss'
 import { imagetools } from '@zerodevx/svelte-img/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import { setDefaultResultOrder } from 'dns'
+
+setDefaultResultOrder('verbatim')
 
 export default defineConfig({
     plugins: [
         sveltekit(),
         purgeCSS(),
         imagetools(),
+        basicSsl(),
         Icons({
             compiler: 'svelte',
         }),

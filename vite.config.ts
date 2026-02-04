1e1c01dc8cadd762251129dafd3d495a31e92e88
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-02-04 22:34:42 +0100

    Remove unused dependency

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import icons from 'unplugin-icons/vite'
 import purgeCSS from 'vite-plugin-tailwind-purgecss'
 import { enhancedImages } from '@sveltejs/enhanced-img'
-// import basicSsl from '@vitejs/plugin-basic-ssl'
 import { setDefaultResultOrder } from 'dns'
 
 setDefaultResultOrder('verbatim')
@@ -13,7 +12,6 @@ export default defineConfig({
         enhancedImages(), // Must come before the SvelteKit plugin
         sveltekit(),
         purgeCSS(),
-        // basicSsl(),
         icons({
             compiler: 'svelte',
         }),

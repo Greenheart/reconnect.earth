cfea5ffe1fd36831d888cf97c1235e72d3292238
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-08-14 00:31:41 +0200

    Use modern configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,20 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 import icons from 'unplugin-icons/vite'
 import { enhancedImages } from '@sveltejs/enhanced-img'
-import { setDefaultResultOrder } from 'dns'
-
-setDefaultResultOrder('verbatim')
+import adapter from '@sveltejs/adapter-static'
+import { resolve } from 'node:path'
 
 export default defineConfig({
   plugins: [
     enhancedImages(), // Must come before the SvelteKit plugin
-    sveltekit(),
+
+    sveltekit({
+      adapter: adapter(),
+      alias: {
+        $components: resolve('src/components'),
+        $assets: resolve('src/assets'),
+      },
+    }),
     icons({
       compiler: 'svelte',
     }),

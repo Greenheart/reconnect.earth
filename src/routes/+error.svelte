cbac8187139e0fba1a0c11615506a9cfe397e045
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-08-14 15:24:23 +0200

    Remove last Skeleton class

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
   import { page } from '$app/state'
+  import { Button } from '$lib/components/ui/button'
   import { SITE_NAME } from '$lib/constants'
 </script>
 
 <div class="flex flex-col items-center justify-center">
   {#if page.status === 404}
     <h2 class="h2 gradient-heading mb-8 font-bold">Sorry, this page does not exist</h2>
-    <a href="/" class="variant-filled-primary btn">Explore {SITE_NAME}</a>
+    <Button href="/">Explore {SITE_NAME}</Button>
   {:else}
     <h2 class="h2 gradient-heading mb-8 font-bold">Oops! Something went wrong</h2>
     <p class="mb-8">
       <span>HTTP {page.status} - </span>
       {page.error?.message}
     </p>
-    <a href="/" class="variant-filled-primary btn">Explore {SITE_NAME}</a>
+    <Button href="/">Explore {SITE_NAME}</Button>
   {/if}
 </div>

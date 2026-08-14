1642bc8bb96a67464ad3a329fba6b951756a7fe9
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-08-14 02:23:31 +0200

    Refactor SectionCard

--- src/components/SectionCard.svelte
+++ src/components/SectionCard.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import { Button } from '$lib/components/ui/button'
+  import * as Card from '$lib/components/ui/card'
+
   interface Props {
     title: string
     text: string
@@ -10,15 +13,13 @@
   let { title, text, href, cta, icon }: Props = $props()
 </script>
 
-<div
-  class="card mx-auto grid max-w-sm grid-rows-[min-content_min-content_min-content_1fr] gap-2 p-4 sm:mx-0"
->
-  {@render icon()}
-  <h3 class="h4 text-primary text-center font-bold">{title}</h3>
-  <p class="mb-2 text-base md:text-lg">
-    {text}
-  </p>
-  <a {href} class="variant-filled-primary btn w-full self-end justify-self-center sm:w-40 md:w-48"
-    >{cta}</a
-  >
-</div>
+<Card.Root>
+  <span class="justify-center">{@render icon()}</span>
+  <Card.Content class="h-full">
+    <Card.Title class="text-primary gradient-heading text-center">{title}</Card.Title>
+    <Card.Description>{text}</Card.Description>
+  </Card.Content>
+  <Card.Footer>
+    <Button class="w-full self-end" {href}>{cta}</Button>
+  </Card.Footer>
+</Card.Root>

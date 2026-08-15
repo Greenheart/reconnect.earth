4a00b49d3b11604847a916fd72b852fe0a6fb3a9
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-08-15 09:58:00 +0200

    Configure `keystatic-sveltekit`

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import { enhancedImages } from '@sveltejs/enhanced-img'
 import adapter from '@sveltejs/adapter-static'
 import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'node:path'
+import { keystatic } from 'keystatic-sveltekit'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     enhancedImages(), // Must come before the SvelteKit plugin
+    keystatic(),
     sveltekit({
       adapter: adapter(),
       alias: {

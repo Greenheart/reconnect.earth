0988c4bd51c91cae954a6c8cecf0671687c6ea81
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-08-22 01:59:04 +0200

    Add resources without tags

--- src/routes/(admin)/admin/cms-config.ts
+++ src/routes/(admin)/admin/cms-config.ts
@@ -1,4 +1,4 @@
-import { AppValidation } from '#lib/content/constants.js'
+import { AppValidation, ResourceValidation } from '#lib/content/constants.js'
 import { type CmsConfig } from '@sveltia/cms'
 
 export default {
@@ -15,6 +15,57 @@ export default {
   media_folder: 'src/assets',
   public_folder: '/',
   singletons: [
+    {
+      name: 'resources',
+      label: '📚️ Resources',
+      file: 'src/data/resources.json',
+      fields: [
+        {
+          name: 'resources',
+          label: 'All resources',
+          widget: 'list',
+          comment:
+            'Tip: To easily find a specific resource to edit it, search in the browser using CTRL+F or CMD+F',
+          fields: [
+            {
+              name: 'title',
+              comment: 'A short descriptive title',
+              maxlength: ResourceValidation.title.max,
+            },
+            {
+              name: 'description',
+              label: 'Description',
+              comment:
+                'Why is this relevant for Reconnect.earth? What are some key topics and insights?',
+              widget: 'text',
+              maxlength: ResourceValidation.description.max,
+            },
+            {
+              name: 'link',
+              label: 'Link',
+              widget: 'string',
+              type: 'url',
+            },
+            {
+              name: 'featured',
+              comment: 'Whether or not this resource should be featured',
+              widget: 'boolean',
+              default: false,
+            },
+            {
+              name: 'quality',
+              label: `Production quality (${ResourceValidation.quality.min}-${ResourceValidation.quality.max})`,
+              comment:
+                "Generally, resources with a higher production quality are preferred to respect people's time. However, sometimes the topic or insights are worth adding despite a lower production quality.",
+              widget: 'number',
+              min: 1,
+              max: 5,
+              default: 3,
+            },
+          ],
+        },
+      ],
+    },
     {
       name: 'apps',
       label: '📱️ Apps',

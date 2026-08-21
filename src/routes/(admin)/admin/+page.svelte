0c1e418c6860f561bf9fb410186eca9072da7798
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-08-21 23:27:36 +0200

    Add notice about limitation of the hot reload

--- src/routes/(admin)/admin/+page.svelte
+++ src/routes/(admin)/admin/+page.svelte
@@ -5,6 +5,9 @@
   init({ config })
 
   if (import.meta.hot) {
+    // TODO: This watches for all changed files in the project
+    // IDEA: Maybe we can refresh only if specific modules are changed?
+    // IDEA: Or maybe we can create a minimal vite plugin to watch for changes for the config file and only then reload the CMS?
     import.meta.hot?.accept(() => {
       console.log('Refreshing after config change…')
 

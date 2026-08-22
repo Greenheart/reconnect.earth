90aee77e1615ac0502e6dc73f78f009e0e80b328
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-08-22 04:41:18 +0200

    Load tags and handle lowercase tagSlugs

--- src/lib/content/resources.remote.ts
+++ src/lib/content/resources.remote.ts
@@ -8,6 +8,7 @@ import rawResources from '#data/resources.json' with { type: 'json' }
 
 export const getResources = prerender(async () => {
   const allTags = await getTags()
+  const combinedTags = [...allTags.MEDIA_TYPES, ...allTags.TOPICS]
   return (
     z
       // The actual data format used by the CMS is slightly different
@@ -16,6 +17,11 @@ export const getResources = prerender(async () => {
       .parse(rawResources)
       .resources.sort(getFeaturedFirst)
       .map((resource) => {
+        resource.tags = resource.tags.map((tagSlug) => {
+          const tag = combinedTags.find((tag) => tagSlug === tag.toLowerCase())
+          if (!tag) throw new Error(`Invalid tag: "${tagSlug}" in resource ${resource.title}`)
+          return tag
+        })
         resource.tags = getSortedTags(resource.tags, allTags)
         return resource
       })
